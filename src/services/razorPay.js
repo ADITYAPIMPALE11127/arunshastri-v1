193832c53d4f38accdef820a6ccb399405347756
Aditya Santosh Pimpale <84523160+ADITYAPIMPALE11127@users.noreply.github.com>
2025-05-31 16:11:56 +0530

    Update razorPay.js

--- src/services/razorPay.js
+++ src/services/razorPay.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Razorpay from 'razorpay';
 import crypto from 'crypto';
-import { saveUserDataToRTDB } from './realtimeDB.js';
+
 
 const router = express.Router();
 
